c958ea44885dbcbde89c7786a70a6ddf78afa6e8
autizm0202 <59321883+autizm0202@users.noreply.github.com>
2022-08-13 18:50:39 +0100

    Add files via upload

---
+++ organize.py
@@ -0,0 +1,35 @@
+import os 
+import shutil
+
+from_dir="C:/Users/FiercePC/OneDrive/Pulpit/everything/hitsound"
+to_dir="C:/Users/FiercePC/OneDrive/Pulpit/everything/lua"
+
+list_of_files= os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+    name,extension=os.path.splitext(file_name)
+    print(name)
+    print(extension)
+    
+    if extension == "":
+        continue 
+    if extension in [".gif",".png",".jpg",".jpeg",".jfif"]:
+        path1 = from_dir + '/' + file_name                       # Example path1 : Downloads/ImageName1.jpg        
+        path2 = to_dir + '/' + "Image_Files"                     # Example path2 : D:/My Files/Image_Files      
+        path3 = to_dir + '/' + "Image_Files" + '/' + file_name   # Example path3 : D:/My Files/Image_Files/ImageName1.jpg
+        #print("path1 " , path1)
+        #print("path3 ", path3)
+
+        # Check if Folder/Directory Path Exists Before Moving
+        # Else make a NEW Folder/Directory Then Move
+        if os.path.exists(path2):
+          print("Moving " + file_name + ".....")
+
+          # Move from path1 ---> path3
+          shutil.move(path1, path3)
+
+        else:
+          os.makedirs(path2)
+          print("Moving " + file_name + ".....")
+          shutil.move(path1, path3)
